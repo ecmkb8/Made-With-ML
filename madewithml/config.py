9f76a7828215c99ec7b0d2e841f25d847c6a17cf
ecmkb8 <ecmkb8@gmail.com>
2023-09-29 10:50:14 -0600

    retry

--- madewithml/config.py
+++ madewithml/config.py
@@ -4,14 +4,15 @@ import os
 import sys
 from pathlib import Path
 
-import mlflow
 import pretty_errors  # NOQA: F401 (imported but unused)
+
+import mlflow
 
 # Directories
 ROOT_DIR = Path(__file__).parent.parent.absolute()
 LOGS_DIR = Path(ROOT_DIR, "logs")
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
-EFS_DIR = Path(f"/efs/shared_storage/madewithml/{os.environ.get('GITHUB_USERNAME', '')}")
+EFS_DIR = Path(f"/Users/evanmoore/Documents/Training/madewithml/{os.environ.get('GITHUB_USERNAME', '')}")
 
 # Config MLflow
 MODEL_REGISTRY = Path(f"{EFS_DIR}/mlflow")
